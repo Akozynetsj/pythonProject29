f86cda6b4359f0d43581fca38869c17ca692a0b9
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 13:52:34 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import logging
+import random
 
-class DataRecorder:
+class RandomNumberGenerator:
     def __init__(self):
-        self.logger = logging.getLogger('DataRecorder')
+        self.logger = logging.getLogger('RandomNumberGenerator')
         self.logger.setLevel(logging.INFO)
-        file_handler = logging.FileHandler('data.log')
+        file_handler = logging.FileHandler('numbers.log')
         file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(file_formatter)
         self.logger.addHandler(file_handler)
 
-    def record_data(self, filename):
+    def generate_numbers(self, filename, count):
         try:
-            data = input("Введіть дані: ")
-            self.logger.info(f"Введені дані: {data}")
             with open(filename, 'w') as file:
-                file.write(data)
-            self.logger.info(f"Дані успішно записано у файл {filename}")
+                for i in range(count):
+                    number = random.randint(1, 100)
+                    file.write(str(number) + '\n')
+                    self.logger.info(f"Згенероване число: {number}")
+            self.logger.info(f"Згенеровані числа успішно записано у файл {filename}")
         except Exception as e:
-            self.logger.error(f"Помилка запису даних у файл {filename}: {str(e)}")
+            self.logger.error(f"Помилка запису згенерованих чисел у файл {filename}: {str(e)}")
 
 
-recorder = DataRecorder()
-filename = 'data.txt'
-recorder.record_data(filename)
+generator = RandomNumberGenerator()
+
+
+filename = 'numbers.txt'
+count = 10
+generator.generate_numbers(filename, count)
